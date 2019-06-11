52c79a15856b27a8123e6de875ec7e7b76e631df
jardesson <agulhars999@gmail.com>
2019-06-11 16:00:02 -0300

    Css Tela Pedido

--- Views/TelaCadPedidos.php
+++ Views/TelaCadPedidos.php
@@ -10,54 +10,56 @@
 	<link rel="stylesheet" type="text/css" href="../css/telapedido.css">
 </head>
 <body>
-	<div class="container" style="position: relative; top: 30px">
-			<h1>Cadastro Pedido</h1>
-			<div class="row">
-				<div id="buscar_cliente">
-					<form id="frmCadProduto">
-						<input type="text" class="form-control input-sm" id="nome_produto" name="nome_produto" placeholder="nome cliente">
-						<span class="btn btn-primary" id="btnBuscarCliente">Buscar</span>
-					</form>
-				</div>
-					<form>
-						<input type="text" class="form-control input-sm" id="codigo_cliente" name="codigo_cliente" placeholder="código produto">
-						<span class="btn btn-danger" id="btnBuscarProduto">Buscar</span><br>
-						<label>Produto:</label>
-						<input type="text" class="form-control input-sm" id="nome_produto" name="nome_produto">
-						<label>Valor:</label>
-						<input type="text" class="form-control input-sm" id="valor_produto" name="valor_produto">
-						<label>Quantidade:</label>
-						<input type="text" class="form-control input-sm" id="quantidade" name="quantidade"><br>
-						<span class="btn btn-danger" id="btnInserirProduto">Inserir Produto</span>
-					</form>
-				</div>
-				<div class="col-sm-4" style="position: relative; margin-left: 30%;">
-					<table border="1">
-						<tr>
-							<td>Código</td>
-							<td>Produto</td>
-							<td>Quantidade</td>
-							<td>Valor</td>
-							<td>Cancelar</td>
-						</tr>
-						<tr>
-							<td>teste</td>
-							<td>teste</td>
-							<td>teste</td>
-							<td>teste</td>
-							<td>teste</td>
-						</tr>
-
-					</table>
-				</div>
-				<div class="col-sm-4" style="position: relative; margin-left: 30%;">
-					<label>Valor Total</label>
-					<input type="text" id="valor_total" name="valor_total">
-					<span class="btn btn-danger" style="position: relative;" id="btnCadastrarProduto">Concluir
-					</span>
-				</div>
+	<div id="container" >
+	 <h1>Cadastro Pedido</h1>
+			<div id="buscar_cliente">
+				<form id="frmCadProduto">
+					<input type="text" class="form-control input-sm" id="nome_cliente" name="nome_cliente" placeholder="nome cliente">
+					<span class="btn btn-primary" id="btnBuscarCliente">Buscar</span>
+				</form>
 			</div>
-		</div>
 
+			<div id="buscar_produto">
+				<input type="text" class="form-control input-sm" id="codigo_produto" name="codigo_produto" placeholder="código produto">
+				<span class="btn btn-danger" id="btnBuscarProduto">Buscar</span><br>		
+			</div>
+			<form id="posiciona_form">
+				<label>Produto:</label>
+				<input type="text" class="form-control input-sm" id="nome_produto" name="nome_produto" placeholder="nome produto"> 
+				<label>Valor:</label>
+				<input type="text" class="form-control input-sm" id="valor_produto" name="valor_produto" placeholder="valor"> 
+				<label>Quantidade:</label>
+				<input type="text" class="form-control input-sm" id="quantidade" name="quantidade" placeholder="Quantidade "><br>
+				<span class="btn btn-danger" id="btnInserirProduto">Inserir Produto</span>
+			</form>
+			<hr id="hrdivision">
+	</div>
+	<div id="posicionaItem">
+	    <div id=""> 
+			<table id="tableitem" border="1">
+				<tr id="cabecaTable">
+					<td>Código</td>
+					<td>Produto</td>
+					<td>Quantidade</td>
+					<td>Valor</td>
+					<td>Cancelar</td>
+				</tr>
+				<tr>
+					<td>teste</td>
+					<td>teste</td>
+					<td>teste</td>
+					<td>teste</td>
+					<td>teste</td>
+				</tr>
+
+			</table>
+		</div>
+			<div id="finalizarCompra">
+				<label>Valor Total</label>
+				<input type="text" id="valor_total" name="valor_total">
+				<span class="btn btn-danger" style="position: relative;" id="btnFinalizarCompra">Concluir
+				</span>
+			</div>
+	</div>
 </body>
 </html>
